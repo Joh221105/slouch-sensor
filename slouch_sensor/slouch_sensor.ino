060f42728994ac6e8f6f7f29950be983692629ac
Joh221105 <101863325+Joh221105@users.noreply.github.com>
2025-02-13 16:41:43 -0500

    initializes additional button variables, changes positional data types to float, implements logic to detect long button press

--- slouch_sensor/slouch_sensor.ino
+++ slouch_sensor/slouch_sensor.ino
@@ -9,14 +9,18 @@ int buzzerPin = 8;          // Buzzer pin
 MPU6050 mpu;                // MPU6050 object
 
 // slouching and straight back position variables
-int straight_x, straight_y, straight_z;
-int slouch_x, slouch_y, slouch_z;
-int current_x, current_y, current_z;
-int threshold_angle = 90;         // angle threshold for slouching, temporary place holder
+float straight_x, straight_y, straight_z;
+float slouch_x, slouch_y, slouch_z;
+float current_x, current_y, current_z;
+float threshold_angle = 0;         // angle threshold for slouching, temporary place holder
 
-bool firstButtonClick = false;
+bool firstButtonClicked = false;
 bool buzzerMode = true;  // default buzzer mode on
-
+unsigned long pressStartTime = 0;
+bool longPressDetected = false;
+const int longPressDuration = 1000;  // 1 second threshold to be considered a long press
+bool initialized = false;
+bool buttonState = false;
 
 void setup() {
   Serial.begin(9600);
@@ -31,5 +35,14 @@ void setup() {
 }
 
 void loop() {
-  
-}
+    if (digitalRead(buttonPin) == LOW) {  
+        if (!buttonState) {
+            buttonState = true;     // acknowledges button press, prevents multiple actions from one button press
+            pressStartTime = millis();     // records start time of button press
+        }
+
+        if (!longPressDetected && millis() - pressStartTime > longPressDuration) {
+            buzzerMode = !buzzerMode;     // toggles between buzzer and LED
+            longPressDetected = true;
+        }
+    } 
